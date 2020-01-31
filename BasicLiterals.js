de2e452d63997932397f2638b0b7ac02f2402860
naima shaikh <naimashaikh550@gmail.com>
2020-01-31 12:02:20 +0500

    'code'

---
+++ BasicLiterals.js
@@ -0,0 +1,13 @@
+const s= 'Hello';
+console.log(s.toUpperCase());
+{/* i can take 's' as a string and used 'to uppercase' javascript method 
+'s.toUpperCase'  seems like a object with a method .  
+*/}
+
+const s1='Hello';
+console.log(typeof s1);
+//It will return string
+
+const s2= new string ('hi');
+console.log(typeof s2);
+//it will return object 
