63c51983f56d597e1305b4e6595145040c20f826
naima shaikh <naimashaikh550@gmail.com>
2020-02-04 15:38:23 +0500

    'code'

--- BasicLiterals.js
+++ BasicLiterals.js
@@ -1,10 +1,8 @@
-import { object } from "prop-types";
-
-const s= 'Hello';
+{/*const s= 'Hello';
 console.log(s.toUpperCase());
 {/* i can take 's' as a string and used 'to uppercase' javascript method 
 's.toUpperCase'  seems like a object with a method .  
-*/}
+
 
 const s1='Hello';
 console.log(typeof s1);
@@ -12,6 +10,7 @@ console.log(typeof s1);
 
 const s2= new string ('hi');
 console.log(typeof s2);
+*/ }
 //it will return object 
 
 //object Literals
@@ -32,7 +31,7 @@ const Book2={
     }
 };
 
-console.log(Book2.getSummary());
-console.log(object.values(Book2));
-console.log(object.keys(Book2));
+//console.log(Book2.getSummary());
+//console.log(Object.values(Book2));
+//console.log(Object.keys(Book2));
 
