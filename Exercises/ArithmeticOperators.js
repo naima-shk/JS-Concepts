522a5c3e85696a4f0aafb61c2b70506333fec9ba
naima shaikh <naimashaikh550@gmail.com>
2020-02-05 11:46:32 +0500

    'code'

---
+++ Exercises/ArithmeticOperators.js
@@ -0,0 +1,11 @@
+//Inside of a console.log(), add 3.5 to your age
+console.log(3.5 + 24);
+
+//On a new line write another console.log(). Inside the parentheses, take the current year and subtract 1969.
+console.log(2020-1969);
+
+//Create another console.log(). Inside the parentheses divide 65 by 240
+console.log(65/240);
+
+//Create one last console.log(). Inside the parentheses, multiply 0.2708 by 100
+console.log(0.2708*100);
