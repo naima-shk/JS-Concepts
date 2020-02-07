ee9d7ff310f07bfa231fbfab506eaec820502d6a
naima shaikh <naimashaikh550@gmail.com>
2020-02-07 12:31:09 +0500

    'code'

---
+++ Exercises/StringInterpolation.js
@@ -0,0 +1,8 @@
+{/* 
+Create a variable called myName and assign it your name.
+Create a variable called myCity and assign it your favorite city’s name.
+Use a single template literal to interpolate your variables into the sentence below. Use console.log()
+*/}
+let myName='naima';
+let myCity='hyd';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
