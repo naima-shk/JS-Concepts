6762672c6b1f613b8449cd392e1a50f7d4414184
naima shaikh <naimashaikh550@gmail.com>
2020-02-17 20:47:23 +0500

    'code'

--- 2_Constructor.js
+++ 2_Constructor.js
@@ -1,16 +1,36 @@
 //constructor
-function Book (title, author,year){
-    this.title=title;
-    this.author=author;
-    this.year=year;
-    this. getSummary = function(){
-        return `${this.title} was written by  ${this.author} in ${this.year}`;
-    }
-};
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getSummary = function() {
+    return `${this.title} was written by  ${this.author} in ${this.year}`;
+  };
+}
 
 //instatiate an object
-const Book1= new Book (' book one ', 'John doe','2013');
-const Book2= new Book (' book two ', 'John doe','2018');
+const Book1 = new Book(" book one ", "John doe", "2013");
+const Book2 = new Book(" book two ", "John doe", "2018");
 
 //console.log(Book2);
 //console.log(Book2.getSummary());
+
+{
+  /* 
+    JS object Constructor
+  */
+}
+// Constructor function for Person objects
+function girl(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+// Create a Person object
+let mysister = new girl("John", "Doe", 45, "brown");
+
+// Display age
+document.getElementById("demo").innerHTML =
+  "My sister  is " + mysister.age + "Her eyes are" + mysister.eyeColor + ".";
--- Exercises/Methods.js
+++ Exercises/Methods.js
@@ -78,3 +78,36 @@ var person = {
   
   // Display data from the object:
   document.getElementById("demo").innerHTML = person.language;
+ {/*
+Object.define property 
+The Object.defineProperty() method can also be used to add Getters and Setters:
+*/}
+// Define an object
+let obj = {counter : 0};
+
+// Define Setters and Getters
+Object.defineProperty(obj, "reset", {
+  get : function () {this.counter = 0;}
+});
+Object.defineProperty(obj, "increment", {
+  get : function () {this.counter++;}
+});
+Object.defineProperty(obj, "decrement", {
+  get : function () {this.counter--;}
+});
+
+Object.defineProperty(obj, "add", {
+  set : function (value) {this.counter += value;}
+});
+Object.defineProperty(obj, "subtract", {
+  set : function (value) {this.counter -= value;}
+});
+
+// Play with counter:
+obj.reset;
+obj.add = 5;
+obj.subtract = 3;
+obj.increment;
+obj.decrement;
+document.getElementById("demo").innerHTML = obj.counter;
+  
