2b774af9465c154afe5555be2d232b3201485086
naima shaikh <naimashaikh550@gmail.com>
2020-02-17 16:17:47 +0500

    'code'

--- Exercises/Methods.js
+++ Exercises/Methods.js
@@ -43,3 +43,38 @@ document.getElementById("vice").innerHTML = girl.fullName();
 
 //</body>
 //</html>
+
+{/*
+  JavaScript Object Accessors (Getters and Setters)
+  This example uses a lang property to get the value of the language property
+ */}
+ // Create an object:
+var person = {
+    firstName: "John",
+    lastName : "Doe",
+    language : "en",
+    get lang() {
+      return this.language;
+    }
+  };
+  
+  // Display data from the object using a getter:
+  document.getElementById("demo").innerHTML = person.lang;
+
+  {/*
+    This example uses a lang property to 'set' the value of the language property
+ */}
+ var person = {
+    firstName: "John",
+    lastName : "Doe",
+    language : "",
+    set lang(lang) {
+      this.language = lang;
+    }
+  };
+  
+  // Set an object property using a setter:
+  person.lang = "en";
+  
+  // Display data from the object:
+  document.getElementById("demo").innerHTML = person.language;
