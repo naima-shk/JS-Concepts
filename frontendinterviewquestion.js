de0fd334e66291fdfae50a56b1242d7fd3f29936
naima shaikh <naimashaikh550@gmail.com>
2020-02-20 19:35:12 +0500

    'code'

--- frontendinterviewquestion.js
+++ frontendinterviewquestion.js
@@ -49,8 +49,21 @@ console.log(test2);
  3) How would you compare two objects in JavaScript?
 ANSWER: Works when you have simple JSON-style objects without methods and DOM nodes inside:
  JSON.stringify(obj1) === JSON.stringify(obj2)
- 
-
-
+ Objects are reference types so you can’t just use === or == to compare 2 objects. One quick way to compare if 2 objects have the same key value, is using JSON.stringify. Another way is using Lodash isEqual function
+ https://www.samanthaming.com/tidbits/33-how-to-compare-2-objects/ 
+ I have found the proper answer of my question through this link . 
 */
+}
+
+//QUESTION 4
+{
+  /*
+  4) As [] is true, [] == true should also be true. right?
+   You are right about first part, [], empty array is an object and object is always truthy. Hence, if you use if([]){console.log('its true')} you will see the log.
+However, special case about == (double equal) is that it will do some implicit coercion.
+Since left and right side of the equality are two different types, JavaScript can't compare them directly . Hence, under the hood, JavaScript will convert them to compare. first right side of the equality will be cooereced to a number and number of true would be 1.
+After that, JavaScript implementation will try to convert [] by usingtoPrimitive (of JavaScript implementation). since [].valueOf is not primitive will use toString and will get ""
+Now you are comparing "" == 1 and still left and right is not same type. Hence left side will be converted again to a number and empty string will be 0.
+Finally, they are of same type, you are comparing 0 === 1 which will be false.
+ */
 }
