0d15a10637506cb568ab539d32a08c9722b8ff4d
naima shaikh <naimashaikh550@gmail.com>
2020-02-24 12:34:42 +0500

    'code'

--- frontendinterviewquestion.js
+++ frontendinterviewquestion.js
@@ -149,3 +149,18 @@ myDate.nextDayName = function (){
 10)21 quick questions that will trick you.
 */
 }
+
+//QUESTION 11
+{
+  /* 11)How could you set a prefix before everything you log? for example, if you log('my message') it will log: "(app) my message"
+   */
+  function log() {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift("(app)");
+    console.log.apply(console, args);
+  }
+
+  log("my message"); //(app) my message
+  log("my message", "your message"); //(app) my message your message
+  //Just get the arguments, convert it to an array and unshift whatever prefix you want to set. Finally, use apply to pass all the arguments to console.
+}
