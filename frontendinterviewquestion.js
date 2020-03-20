3ae25bee8c33ef7a07827ba93c978f5e2d2ce573
naima shaikh <naimashaikh550@gmail.com>
2020-03-20 16:08:59 +0500

    'code'

--- frontendinterviewquestion.js
+++ frontendinterviewquestion.js
@@ -219,3 +219,20 @@ Solution: You can fix it by avoiding closure. Just create a IIFE (Immediately In
 //Question 14
 /* 14)Look at the code below, I have a property in a object and I am creating a new object where I am setting it to a new value. If I delete that property what will i get if I try to access that property?
  */
+let myObject = {
+  price: 20.99,
+  get_price: function() {
+    return this.price;
+  }
+};
+let customObject = Object.create(myObject);
+customObject.price = 19.99;
+
+delete customObject.price;
+console.log(customObject.get_price());
+//EXPLANATION
+/* 
+Explanation: This is very interesting question. When you create object.create(myObject) you are creating new object where the myObject will be the parent of the newly created object. Hence the price property will be at the parent.
+
+When you are assigning some value to customObject.price, you are creating a new property on the child. This means, when you delete customObject.price it deletes the price property in the customObject (in the child). However, when you call the method getprice, first it looks for this.price in the child since the customObject doesn't have price property, JavaScript executor walks through the prototype chain towards the parent. Since customObject was inherited from myObject and myObject has a price property, the get_price method returns the price from parent. Hence, you are getting 20.99
+*/
