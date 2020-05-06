09e5d5c9a224085510bb0c6687ab1bc1cff8e0fc
naima shaikh <naimashaikh550@gmail.com>
2020-05-07 00:32:07 +0500

    'code-academy-project'

---
+++ Magic Eight Ball.js
@@ -0,0 +1,42 @@
+let userName = " ";
+userName ? console.log(`Hello ${userName}`) : console.log("Hello!");
+
+const userQuestion = " ";
+console.log(userQuestion);
+const randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+switch (eightBall) {
+  case 0:
+    //console.log('It is certain');
+    eightBall = "It is certain";
+    break;
+  case 1:
+    //console.log('It is decidedly so');
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    //console.log('Reply hazy try again');
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    // console.log('Cannot predict now');
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    // console.log('Do not count on it');
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    // console.log('My sources say no');
+    eightBall = "My sources say no";
+    break;
+  case 6:
+    // console.log('Outlook not so good');
+    eightBall = "outlook not so good";
+    break;
+  case 7:
+    // console.log('Signs point to yes');
+    eightBall = "Signs point to yes";
+    break;
+    console.log(eightBall);
+}
